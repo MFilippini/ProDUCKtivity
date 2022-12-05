601ff79c10dfad0b88fc506e025babf6512257d0
courtf68 <c.fenderson@wustl.edu>
2022-12-04 18:45:34 -0600

    accidentally started w o pulling

--- ProDUCKtivity/ViewController.swift
+++ ProDUCKtivity/ViewController.swift
@@ -62,6 +62,28 @@ class ViewController: UIViewController {
         }
     }
     
+    
+    
+    //sec adding in menu
+    
+    let usersItem = UIAction(title: "Users", image: UIImage(systemName: "person.fill")) { (action) in
+
+             print("Users action was tapped")
+        }
+
+        let addUserItem = UIAction(title: "Add User", image: UIImage(systemName: "person.badge.plus")) { (action) in
+
+            print("Add User action was tapped")
+        }
+
+        let removeUserItem = UIAction(title: "Remove User", image: UIImage(systemName: "person.fill.xmark.rtl")) { (action) in
+             print("Remove User action was tapped")
+        }
+
+        let menu = UIMenu(title: "My Menu", options: .displayInline, children: [usersItem , addUserItem , removeUserItem])
+  
+    //end sec
+    
     func displayUnfocusedState(){
         timerLabel.isHidden = true
         messageLabel.text = "Get your ducks in a row!"
